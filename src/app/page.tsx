51877e29089af460a4af70d0974568b43080cbed
Edgar Tolete <edgartolete@outlook.com>
2025-12-06 18:17:26 +0800

    cleanup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,23 +2,9 @@
 
 export const dynamic = "force-static"
 
-import { authClient } from "@/utils/auth-client";
-import Button from "@mui/material/Button";
-import { useRouter } from "next/navigation";
-
 export default function Home() {
-  const router = useRouter()
-
-  const handleLogout = async () => {
-    const res = await authClient.signOut()
-
-    if(res.data?.success){
-      router.push('/login')
-    }
-
-  }
+  
   return <div>
-  <Button onClick={handleLogout}>Logout</Button>
 
   </div>;
 }
